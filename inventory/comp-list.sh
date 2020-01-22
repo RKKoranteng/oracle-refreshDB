a44bfedcd749203096f894864a22269b214ad7ce
Richard Koranteng <richard@rkkoranteng.com>
2020-01-22 11:42:39 -0500

    Create comp-list.sh

---
+++ inventory/comp-list.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# inventory of components
+
+#[database] [component script]  [description]
+example         comp-example.sh         component used for demo
+CWDB01          comp-CWDB01.sh          component used for testing
